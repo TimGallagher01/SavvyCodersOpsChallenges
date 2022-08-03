ea0e5edf632ad39b684c50db7d75062a6c651653
Tim Gallagher <64342761+TimGallagher01@users.noreply.github.com>
2022-08-02 20:02:10 -0500

    Add files via upload

---
+++ OpsChallenge1.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+# Script Name: class04.sh
+# Author Name: Tim Gallagher
+# Date of revision:  07/14/2022
+# Description of purpose: Ops Challenge 04 - Array
+# Ecample usage:  $bash class 04.sh
+# Expected output: Create 4 directories and 1 file in each
+# Declaration of variables (if any): paths
+# Declaration of functions (if any): n/a
+
---
+++ demo.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+
+# Script Name:  demo.sh
+# Author Name: Tim Gallagher
+# Date of Revision: 07/11/2022
+# Description of purpose:   First BASH Script
+# Example usage: : $demo.sh
+# Expected output:  "I made a script"
+# Declaration of variables (if any):    $str_textline
+# Declaration of functions (if any):    
+
+str_textline="Guess I did it"
+
+# Main
+echo $str_textline
+
+# End
